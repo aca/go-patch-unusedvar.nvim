a87cec9e97c2cd4290434b802899b1377b0440cc
rok <acadx0@gmail.com>
2023-01-23 20:45:20 +0900

    replace lsp.format

---
+++ example.go
@@ -0,0 +1,7 @@
+package main
+
+func main(){
+    x, y := 3, 4
+    _ = x
+
+}
--- lua/go-patch-unusedvar/init.lua
+++ lua/go-patch-unusedvar/init.lua
@@ -3,14 +3,17 @@ vim.api.nvim_create_user_command("GoPatchUnusedVar", function()
 	local buf = 0
 	for _, v in ipairs(vim.diagnostic.get(buf, { severity = vim.diagnostic.severity.ERROR })) do
 		if v["code"] == "UnusedVar" then
-            local nod = vim.treesitter.get_node_at_pos(buf, v.lnum, v.col, { lang = "go" })
-            local var_name = vim.treesitter.query.get_node_text(nod, 0, {})
+            local cur_node = vim.treesitter.get_node_at_pos(buf, v.lnum, v.col, { lang = "go" })
+
+            -- TODO: handle error cases
+            local var_name = vim.treesitter.query.get_node_text(cur_node, 0, {})
 
             -- "append line at the end of declaration"
-            local end_row  = nod:parent():parent():end_()
-			vim.api.nvim_buf_set_lines(buf, end_row + 1, end_row + 1, true, { '_ = ' .. var_name })
-
-            vim.lsp.buf.format()
+            local block = cur_node:parent():parent()
+            local _, start_col  = block:start()
+            local end_row  = block:end_()
+			vim.api.nvim_buf_set_lines(buf, end_row + 1, end_row + 1, true, { string.rep(' ', start_col) .. '_ = ' .. var_name })
+            -- vim.lsp.buf.format()
 		end
 	end
 end, {})
